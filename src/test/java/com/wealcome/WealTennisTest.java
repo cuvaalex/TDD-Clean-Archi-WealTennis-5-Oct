2eb66938b3f73f33416fd330ae1bb3b158923ea2
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2020-10-05 20:24:49 +0200

    start first WealTennis test

---
+++ src/test/java/com/wealcome/WealTennisTest.java
@@ -0,0 +1,22 @@
+package com.wealcome;
+
+import org.junit.jupiter.api.Test;
+
+public class WealTennisTest {
+
+    // 0   15    30     40   Win
+    // 40 - 40 =>   0   15   30  40 Advantage Win
+
+    // 0 0   Love All
+    // 15 0    Fifteen Love
+    // 15 15    Fifteen All
+    // 30 15    Thirty Fifteen
+    // 40 15    Forty Fifteen
+    // Won
+
+    @Test
+    void doNothing() {
+
+    }
+
+}
